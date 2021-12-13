9bbeca4f1c1766e53bbb304f52f23ffd226af017
Masadavide <davide.masadm@gmail.com>
2021-12-13 18:55:24 +0100

    correzione con funzione

--- myscript.js
+++ myscript.js
@@ -3,27 +3,21 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
-/* function myFunction() {
+function myFunction() {
     let myEmail = document.getElementById("myEmail").value;
-    document.getElementById("demo").innerHTML = myEmail;
-    return myEmail;
-}
-let myEmail2 = myFunction(); */
-
-const myEmail = prompt("Inserisci la tua email:");
-
-const validEmail = ["gee_male@gmail.com","pope_francis@franciscus.com","elon_musk@X_Æ_A-12.com"];
-
-let trovata = false;
-
-for (let i = 0; i < validEmail.length; i++){
-    if(myEmail == validEmail[i]){
-        trovata = true;
+    const validEmail = ["gee_male@gmail.com","pope_francis@franciscus.com","elon_musk@X_Æ_A-12.com"];
+    
+    let trovata = false;
+    
+    for (let i = 0; i < validEmail.length; i++){
+        if(myEmail == validEmail[i]){
+            trovata = true;
+        }
+    }    
+    
+    if(trovata){
+        document.getElementById("demo").innerHTML = "Email corretta.";
+    }else{
+        document.getElementById("demo").innerHTML = "Email errata.";
     }
-}    
-
-if(trovata){
-    console.log("Email corretta.");
-}else{
-    console.log("Email errata.");
 }
