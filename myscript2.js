2e8a1bf8560bf422343606347d3c6b9b58ca1e14
Masadavide <davide.masadm@gmail.com>
2021-12-13 19:11:07 +0100

    finito

--- myscript2.js
+++ myscript2.js
@@ -8,13 +8,19 @@ Se dobbiamo confrontare qualcosa che “cosa” ci serve? */
 let dado_utente = Math.floor (Math.random() * 6 + 1);
 let dado_cpu = Math.floor (Math.random() * 6 + 1);
 
+document.getElementById("dado1").innerHTML = dado_utente;
+document.getElementById("dado2").innerHTML = dado_cpu;
+
 console.log(dado_utente)
 console.log(dado_cpu)
 
 if(dado_utente > dado_cpu){
+    document.getElementById("risultato").innerHTML = ("Hai vinto");
     console.log("Hai vinto");
 }else if(dado_utente < dado_cpu){
+    document.getElementById("risultato").innerHTML = ("Hai perso");
     console.log("Hai perso");
 }else{
+    document.getElementById("risultato").innerHTML = ("Parità");
     console.log("Parità");
 }
